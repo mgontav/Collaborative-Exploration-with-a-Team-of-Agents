b3b75d69d2955e16167b513c179e35c5f8387fd9
Miguel Tavares <mgt@student.dei.uc.pt>
2011-01-25 17:56:02 +0000

    Prototypes added.

---
+++ objects/Prototype.java
@@ -0,0 +1,32 @@
+package sim.app.exploration.objects;
+
+import java.awt.Color;
+
+import sim.app.exploration.utils.Utils;
+
+/**
+ * This is not an object.
+ * Thank you.
+ * @author Miguel
+ *
+ */
+public class Prototype {
+
+	public Class thisClass;
+	public double size;
+	public Color color;
+	public int nOccurrs;
+	
+	public Prototype(Class cls, double s, Color c){
+		this.thisClass = cls;
+		this.size = s;
+		this.color = c;
+		this.nOccurrs = 1;
+	}
+	
+	public void addOccurrence(double s, Color c){
+		this.size = (this.size + s)/2.0;
+		this.color = Utils.avgColor(this.color, c);
+		this.nOccurrs += 1;
+	}
+}
